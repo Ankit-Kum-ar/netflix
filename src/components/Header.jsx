755e1f1775633355be6e7ccf8d41ed2481458e38
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-18 17:28:05 +0530

    Absolute Problem has solved for signIn and signOut

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,16 @@ import { FaUser } from "react-icons/fa";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../redux/Slices/userSlice";
-const Header = () => {
+const Header = ({absoluteValue}) => {
 
     const navigate = useNavigate();
     const user = useSelector((state) => state.user.user);
     const dispatch = useDispatch();
+    const [isAbsolute, setIsAbsolute] = useState(false);
 
     // Call the API of onAuthStateChange (this is like an event listener, so called at once).
     // At once can be done by useEffect().
@@ -25,6 +26,7 @@ const Header = () => {
           // Here we dispatch the user to store.
           dispatch(addUser({uid,email,displayName}));
           navigate('/browser')
+          setIsAbsolute(true)
           
   
         } else { // When user Sign-Out then this condition execute.
@@ -32,6 +34,7 @@ const Header = () => {
           // Here we dispatch the user to store.
           dispatch(removeUser());
           navigate('/')
+          setIsAbsolute(false);
         }
       });
     }, [])
@@ -44,13 +47,13 @@ const Header = () => {
       }); 
     }
     return (
-      <div>
-        <div className="md:pl-32 pl-3 py-2 bg-gradient-to-b from-gray-950 flex justify-between">
-          <img className="md:w-48 w-32" src={logo} alt="logo" />
+      <div className={` ${isAbsolute ? 'bg-black  top-0 w-screen absolute' : ''}`}>
+        <div className="md:pl-14 pl-3 py-2 bg-gradient-to-b from-gray-950 flex justify-between">
+          <img className="md:w-48 w-28" src={logo} alt="logo" />
           {
             // Check if user sign in or sign out, bcz userSlice is null if user is signOut.
             user ? (
-              <div className="my-4 pr-12">
+              <div className="my-4 mr-12 ">
                 <button className="bg-red-700 p-2 px-4 rounded-md text-white font-semibold flex gap-2" onClick={handleSignOut}>
                   <span className="my-1"><FaUser /></span>
                   Sign Out
