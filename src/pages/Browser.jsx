60ebf3df7beb01e21a7054854d70b0caf68dedab
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-19 13:58:48 +0530

    Calling all custom hooks for fetching API data

--- src/pages/Browser.jsx
+++ src/pages/Browser.jsx
@@ -3,8 +3,18 @@ import Header from '../components/Header'
 import { useNowPlayingMovies } from '../hooks/useNowPlayingMovies'
 import MainContainer from '../components/MainContainer';
 import SecondaryContainer from '../components/SecondaryContainer';
+import { usePopularMovies } from '../hooks/usePopularMovies';
+import { useTopRatedMovies } from '../hooks/useTopRatedMovies';
+import { useUpcomingMovies } from '../hooks/useUpcomingMovies';
+import { useTvShows } from '../hooks/useTvShows';
 const Browser = () => {
+
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
+  useTvShows();
+
   return (
     <div>
       <Header/>
