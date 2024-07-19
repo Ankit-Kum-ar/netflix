bed5af2bc82425ba41e6baf4adc83879140f65be
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-20 01:11:35 +0530

    Placeholder Color make black

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      placeholderColor: {
+        'black': '#000000',
+      },
+    },
   },
   plugins: [],
 }
