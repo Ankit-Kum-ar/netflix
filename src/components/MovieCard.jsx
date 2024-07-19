a5f6aad39479995cd85badf837ca3502f066b917
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-19 18:30:53 +0530

    Image size fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import { MOVIECARD_IMAGE_URL } from '../utils/constant'
 const MovieCard = ({poster}) => {
   // console.log(poster_path);
   return (
-    <div className='md:w-44 w-[70px] pr-2'>
+    <div className='md:w-44 w-24 pr-2'>
        <img src={MOVIECARD_IMAGE_URL + poster} alt="" />
     </div>
   )
