60ad437d9685ffdf02c22d597db1e63433b80dc9
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-19 13:52:27 +0530

    Using Top Rated Movies API

---
+++ src/hooks/useTopRatedMovies.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import { API_OPTIONS } from "../utils/constant"
+import { useDispatch } from "react-redux"
+import { addTopRatedMovies } from "../redux/Slices/movieSlice";
+
+export const useTopRatedMovies = () => {
+
+    const dispatch = useDispatch();
+    const getTopRatedMovies = async() => {
+        // Fetching Data from TMB API and update the store
+        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', API_OPTIONS)
+        const data = await response.json()
+        console.log(data?.results)
+        // Dispatch the action to set the movies
+        dispatch(addTopRatedMovies(data?.results));
+    }
+    useEffect(() => {
+        getTopRatedMovies();
+    }, [])
+}
