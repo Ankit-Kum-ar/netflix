fc31341b9031038723b489c88c01a82f91b227fb
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-19 13:58:22 +0530

    Chaning CSS of buttons

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -8,10 +8,10 @@ const VideoTitle = (props) => {
       <h1 className='text-4xl font-semibold text-slate-200 mb-[1%]'>{props.title}</h1>
       <p className='w-7/12 mb-[1%] '>{props.overview}</p>
       <div className=' flex gap-4'>
-        <button className='bg-white hover:bg-gray-400 shadow-md flex text-black px-6 my-2 py-2'>
+        <button className='bg-white hover:bg-gray-400 shadow-md rounded-md flex text-black px-6 my-2 py-2'>
             <span className='my-1 px-2'><FaPlay /></span>  Play 
         </button>
-        <button className=' flex bg-gray-500 shadow-md text-white px-3 my-2 py-2'>
+        <button className=' flex bg-gray-500 hover:bg-gray-600 rounded-md shadow-md text-white px-3 my-2 py-2'>
             <span className='my-1 px-2'><IoIosInformationCircleOutline /></span>  More Info 
         </button>
 
