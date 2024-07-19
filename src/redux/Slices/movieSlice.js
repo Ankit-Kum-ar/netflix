edc33e77fd4931c47ea8f5e50c491e7ed168a336
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-19 13:54:46 +0530

    Adding different reducers and actions for different API data storage

--- src/redux/Slices/movieSlice.js
+++ src/redux/Slices/movieSlice.js
@@ -4,11 +4,27 @@ const movieSlice = createSlice({
     name: 'movie',
     initialState: {
         nowPlayingMovies: [],
-        trailerVideo: null
+        trailerVideo: null,
+        popularMovies: null,
+        topRatedMovies: null,
+        upcomingMovies: null,
+        tvShows: null
     },
     reducers: {
         addNowPlayingMovies: (state, action) => {
             state.nowPlayingMovies = action.payload
+        },
+        addPopularMovies: (state, action) => {
+            state.popularMovies = action.payload
+        },
+        addTopRatedMovies: (state, action) => {
+            state.topRatedMovies = action.payload
+        },
+        addUpcomingMovies: (state, action) => {
+            state.upcomingMovies = action.payload
+        },
+        addTvShows: (state, action) => {
+            state.tvShows = action.payload
         },
         addTrailerVideo: (state, action) => {
             state.trailerVideo = action.payload
@@ -16,5 +32,5 @@ const movieSlice = createSlice({
     }
 })
 
-export const {addNowPlayingMovies , addTrailerVideo} = movieSlice.actions
+export const {addNowPlayingMovies , addTrailerVideo, addPopularMovies, addTopRatedMovies, addUpcomingMovies, addTvShows} = movieSlice.actions
 export default movieSlice.reducer
